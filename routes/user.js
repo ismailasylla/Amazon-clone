d7e30e5789c1ef499cb87d8a0082b1171735289d
ismaila sylla <syldox@gmail.com>
2017-10-10 22:48:56 +0530

    028 Adding an Edit feature Profile

--- routes/user.js
+++ routes/user.js
@@ -65,4 +65,25 @@ router.post('/signup', function(req, res, next)
         req.logout();
         res.redirect('/');
     });
+
+
+    router.get('/edit-profile', function(req, res, next){
+        res.render('accounts/edit-profile.ejs', { message: req.flash('success')});
+    });
+
+    router.post('/edit-profile', function(req, res, next){
+        User.findOne({ _id: req.user._id }, function(err, user){
+            if(err) return next(err); 
+            if(req.body.name) user.profile.name = req.body.name;
+            if(req.body.address) user.address = req.body.address;
+
+            user.save(function(err){
+                if (err) return next(err);
+                req.flash('success', 'Profile Successfully Edited');
+                return res.redirect('/edit-profile');
+            });
+
+        })
+    });
+
     module.exports = router;
