374422c15be183bda9de1caae0f4bc3c8d7cf5df
ismaila sylla <syldox@gmail.com>
2017-10-10 23:01:02 +0530

    cat and prod models

---
+++ models/product.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var productSchema = new Schema({
+    category:{ type: Schema.Types.ObjectId, ref: 'Category'},
+    name: String,
+    price: Number,
+    image: String
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
+
